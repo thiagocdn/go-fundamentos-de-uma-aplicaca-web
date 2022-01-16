043ca347af35fb29fc245ec702a21244a53a83e0
Thiago Cedran Santos <thiagocdn@gmail.com>
2022-01-16 14:38:05 -0300

    Criado conexao com DB

---
+++ go.mod
@@ -0,0 +1,5 @@
+module go-fundamentos-aplicacao-web
+
+go 1.17
+
+require github.com/lib/pq v1.10.4 // indirect
--- main.go
+++ main.go
@@ -1,9 +1,23 @@
 package main
 
 import (
+	"database/sql"
 	"html/template"
 	"net/http"
+
+	_ "github.com/lib/pq"
 )
+
+func connectaComBancoDeDados() *sql.DB {
+	conexao := "user=postgres dbname=alura_loja password=123456 host=localhost sslmode=disable"
+	db, err := sql.Open("postgres", conexao)
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	return db
+}
 
 type Produto struct {
 	Nome       string
@@ -15,6 +29,9 @@ type Produto struct {
 var temp = template.Must(template.ParseGlob("templates/*.html"))
 
 func main() {
+	db := connectaComBancoDeDados()
+	defer db.Close()
+
 	http.HandleFunc("/", index)
 	http.ListenAndServe(":3000", nil)
 }
