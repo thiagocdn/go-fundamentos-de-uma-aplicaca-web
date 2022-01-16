c34f747c0f141ff990ea2ffc8bc0d318c219c232
Thiago Cedran Santos <thiagocdn@gmail.com>
2022-01-16 12:21:17 -0300

    Adicionado dinamicidade ao projeto

--- main.go
+++ main.go
@@ -5,13 +5,26 @@ import (
 	"net/http"
 )
 
+type Produto struct {
+	Nome       string
+	Descricao  string
+	Preco      float64
+	Quantidade int
+}
+
 var temp = template.Must(template.ParseGlob("templates/*.html"))
 
 func main() {
 	http.HandleFunc("/", index)
-	http.ListenAndServe(":8000", nil)
+	http.ListenAndServe(":3000", nil)
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
-	temp.ExecuteTemplate(w, "Index", nil)
+	produtos := []Produto{
+		{Nome: "Camiseta", Descricao: "Azul", Preco: 39, Quantidade: 5},
+		{"Tenis", "Confortavel", 89, 10},
+		{"Fone", "Intrauricular", 120, 2},
+	}
+
+	temp.ExecuteTemplate(w, "Index", produtos)
 }
