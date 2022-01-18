117767c79eff4000f51139b3e5f2294990ffa587
Thiago Cedran Santos <thiagocdn@gmail.com>
2022-01-17 22:25:00 -0300

    Finalizado adição de produto no db

--- models/produtos.go
+++ models/produtos.go
@@ -49,4 +49,12 @@ func BuscaTodosOsProdutos() []Produto {
 func CriarNovoProduto(nome, descricao string, preco float64, quantidade int) {
 	db := db.ConectaComBancoDeDados()
 
+	insereDadosNoBanco, err := db.Prepare("insert into produtos(nome, descricao, preco, quantidade) values ($1, $2, $3, $4)")
+	if err != nil {
+		panic(err.Error())
+	}
+
+	insereDadosNoBanco.Exec(nome, descricao, preco, quantidade)
+
+	defer db.Close()
 }
